eb61357a6d055f89cc1935bf1b9bd20162eb9c30
oscarhurtado2021 <86067285+oscarhurtado2021@users.noreply.github.com>
2022-03-25 16:11:35 -0500

    Add files via upload

---
+++ lab2/ejer1.js
@@ -0,0 +1,11 @@
+<div id="script">
+    <script>
+        
+        let nombre =prompt('Ingrese su nombre de usuario: ');
+        let email =prompt('Ingrese su correo: ');
+        document.write('<br/>');
+        document.write('Nombre de usuario : ' + nombre);
+        document.write('<br/>')
+        document.write('Correo:' + email);
+    </script>
+</div>  
